a5c8e503a428c5e5e8e9172f7142007f7adb476e
programmershakib <programmershakibinfo@gmail.com>
2024-09-20 02:35:12 +0600

    first commit

---
+++ js/utilities.js
@@ -0,0 +1,17 @@
+/***
+ * common Shared Function Here
+ */
+// console.log('utilities loaded');
+
+
+// function getInputFieldValueById() {
+//     console.log('will get value by id');
+//     const addMoney = document.getElementById('input-add-money').value;
+//     return addMoney;
+// }
+
+
+function getInputFieldValueById(id) {
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
+}
